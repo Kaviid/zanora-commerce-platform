ed1b1de89377efa6fbd790b6f58d08a9ce9d7e82
Kaveesha Dias <kaveeshadias27@gmail.com>
2026-03-17 00:13:22 +0530

    Build product details page hero section and build dropdown and crop some icon img and structered some files

---
+++ js/product-details-page/dropdown.js
@@ -0,0 +1,26 @@
+const dropdown = document.querySelector(".dropdown");
+const select = dropdown.querySelector(".select");
+const menu = dropdown.querySelector(".menu");
+const selected = dropdown.querySelector(".selected");
+const options = dropdown.querySelectorAll(".menu li");
+
+// open dropdown
+select.addEventListener("click", (e) => {
+  menu.classList.toggle("menu-open");
+  e.stopPropagation();
+});
+
+// choose option
+options.forEach(option => {
+  option.addEventListener("click", () => {
+    selected.textContent = option.textContent;
+    menu.classList.remove("menu-open");
+  });
+});
+
+// close when clicking outside
+document.addEventListener("click", (e) => {
+  if (!dropdown.contains(e.target)) {
+    menu.classList.remove("menu-open");
+  }
+});
